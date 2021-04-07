149511581cf0714779b47304c2aec1b5996e9ca6
Abon Ayodeji <abonayodeji@gmail.com>
2021-04-07 16:49:08 +0100

    'changing'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-const appError = require('./utils/catchAsync');
+const appError = require('./utils/appError');
 dotenv.config({path: './config.env'});
 
 
