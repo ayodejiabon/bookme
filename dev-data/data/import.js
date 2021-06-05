f57be2c778b26556d514c1067995f96848bd02a5
Abon Ayodeji <abonayodeji@gmail.com>
2021-06-05 22:07:55 +0100

    revert

--- dev-data/data/import.js
+++ dev-data/data/import.js
@@ -18,34 +18,34 @@ mongoose.connect(DB, {
 	console.log("DB connection succcessfull")
 })
 
-// const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'UTF-8'));
-// const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'UTF-8'));
-// const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'UTF-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'UTF-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'UTF-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'UTF-8'));
 
-// const importData = async () => {
-// 	try{
-// 		await Tour.create(tours);
-// 		await User.create(users, {validateBeforeSave:false});
-// 		await Review.create(reviews);
-// 		console.log("Nice one g");
-// 	}catch (err){
-// 		console.log(err);
-// 	}
-// }
-// const deletdata = async () => {
-// 	try{
-// 		await Tour.deleteMany();
-// 		await User.deleteMany();
-// 		await Review.deleteMany();
-// 		console.log("Deleted");
-// 	}catch (err){
-// 		console.log(err);
-// 	}
-// }
-// importData();
-// if (process.env[2] === '--import') {
-// 	importData()
-// }
-// if (process.env[2] === '--delete') {
-// 	deletdata()
-// }
+const importData = async () => {
+	try{
+		await Tour.create(tours);
+		await User.create(users, {validateBeforeSave:false});
+		await Review.create(reviews);
+		console.log("Nice one g");
+	}catch (err){
+		console.log(err);
+	}
+}
+const deletdata = async () => {
+	try{
+		await Tour.deleteMany();
+		await User.deleteMany();
+		await Review.deleteMany();
+		console.log("Deleted");
+	}catch (err){
+		console.log(err);
+	}
+}
+importData();
+if (process.env[2] === '--import') {
+	importData()
+}
+if (process.env[2] === '--delete') {
+	deletdata()
+}
